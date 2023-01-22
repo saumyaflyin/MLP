ca982c297d438b7f62522375f76abafae3562553
Kumari <saumya.c.kumari@accenture.com>
2023-01-22 21:12:30 +0530

    setup file completed

---
+++ data_dump.py
@@ -0,0 +1,24 @@
+!pip install pymongo
+
+import pymongo
+import pandas as pandas
+import json
+
+client = pymongo.MongoClient("mongodb+srv://saumya:RandomForest@123@saumya.bur55.mongodb.net/?retryWrites=true&w=majority")
+#db = client.test
+
+DATA_FILE_PATH = {r"C:\Users\saumya.c.kumari\OneDrive - Accenture\Desktop\New folder\MLP\MLP\insurance.csv"}
+DATABASE_NAME = "INSURANCE"
+COLLECTION_NAME = "INSURANCE_PROJECT"
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and Columns: {df.shape}")
+
+    df.reset_index(drop = True, inplace = True)
+
+    json_record = list(json.load(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
