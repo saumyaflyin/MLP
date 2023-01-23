700f56a9a1edecab73983671a986111baa48b2c0
Kumari <saumya.c.kumari@accenture.com>
2023-01-23 15:37:39 +0530

    config and artifact updated

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from Insurance.exception import InsuranceException
 import os
 import sys
 from Insurance.utils import get_collection_as_dataframe
+from Insurance.entity.config_entity import DataIngestionConfig
+from Insurance.entity import config_entity
 
 def test_logger_exception():
     try:
@@ -18,7 +20,10 @@ if __name__ == "__main__":
     try:
         #test_logger_exception()
         #start_training_pipeline()
-        get_collection_as_dataframe(database_name = "INSURANCE", collection_name= "INSURANCE_PROJECT")
+        #get_collection_as_dataframe(database_name = "INSURANCE", collection_name= "INSURANCE_PROJECT")
+         training_pipeline_config= config_entity.TrainingPipelineConfig()
+         data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config)
+         print(data_ingestion_config.to_dict())
     except Exception as e:
         print(e)
 
